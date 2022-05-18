265044d2db0eb28d6a34e70edf566b4b9fcb9f0f
Win Feigle <win.feigle@gmail.com>
2022-05-17 18:40:43 -0600

    commit tree 9aa61a9604350b4ec5b7ae7df4b9ff8288b88031

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Win Feigle'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`
